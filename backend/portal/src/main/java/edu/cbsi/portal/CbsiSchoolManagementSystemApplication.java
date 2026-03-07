7b847c982a97dbaabf78dc9afc728247dc892d25
LAB13_USER ON WS40 <L13Y09W40@LabHCI.net>
2026-03-07 08:45:56 +0800

    chore: set up application properties

---
+++ backend/portal/src/main/java/edu/cbsi/portal/CbsiSchoolManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.cbsi.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsiSchoolManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbsiSchoolManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/portal/src/test/java/edu/cbsi/portal/CbsiSchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cbsi.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbsiSchoolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
